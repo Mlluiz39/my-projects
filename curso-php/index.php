b7c2f23aee0f486f6d4793b8849fc820d2b536e8
Mlluiz39 <mlluizpereira39@gmail.com>
2021-03-02 21:07:03 -0300

    add pasta php

---
+++ curso-php/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document in PHP</title>
+</head>
+
+<body>
+    <?php
+        echo "Marcelo Luiz Site em php";
+    ?>
+    <br>
+    <?= "Utilizando a tag de impresão" ?>   
+</body>
+</html>
+
+
+
+
+
+
