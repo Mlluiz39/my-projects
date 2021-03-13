f95c2d31f5759c00f8a5e102fbd85052be9ec20b
Mlluiz39 <mlluizpereira39@gmail.com>
2021-03-13 17:42:54 -0300

    alteração do index js

--- notasFaculdade/index.js
+++ notasFaculdade/index.js
@@ -1,42 +1,25 @@
-const calcular = document.querySelector("#calcular");
-const calcularN2 = document.querySelector("#calcularN2");
-const mediaFinal = document.querySelector("#mediaFinal");
+function mediaTotal() {
 
-function calculaN1() {
   let at1 = document.querySelector("#a1").value;
   let at2 = document.querySelector("#a2").value;
   let at3 = document.querySelector("#a3").value;
   let at4 = document.querySelector("#a4").value;
-  let resultado =
-    ((parseFloat(at1) + parseFloat(at2) + parseFloat(at3) + parseFloat(at4)) /
-      4) *
-    0.4;
 
-  document.querySelector(
-    "#resultado"
-  ).textContent = `Média da N1 é = ${resultado.toFixed(1)}`;
-}
+  let resultado = ((parseFloat(at1) + parseFloat(at2) + parseFloat(at3) + parseFloat(at4)) / 4) * 0.4;
 
-function calculaN2() {
-  let n2f = document.querySelector("#n2").value;
+  let at22 = document.querySelector("#n2").value;
+  let n2f = at22 * 0.6;
 
-  let resultadoN2 = n2f * 0.6;
+  let total = parseFloat(resultado) + parseFloat(n2f);
 
-  document.querySelector(
-    "#resultadoN2"
-  ).textContent = `Média da N2 é = ${resultadoN2.toFixed(1)}`;
-}
+  if (total >= 6) {
+    return document.querySelector("#resultadofinal").textContent = `Aprovado com a média ${total}`
+  } else {
+    return document.querySelector("#resultadofinal").textContent = `Reprovado com a média ${total}`
+  }
+};
 
-function calculoFinal() {
-  let mediafn1 = document.querySelector("#mf1").value;
-  let mediafn2 = document.querySelector("#mf2").value;
+document.getElementById('confirm').onreset = function () {
 
-  let resultadofinal = parseFloat(mediafn1) + parseFloat(mediafn2);
-
-  document.querySelector(
-    "#resultadofinal"
-  ).textContent = `Sua Média final é ${resultadofinal.toFixed(1)}`;
-}
-
- 
- 
+  return confirm ("Gostaria de apagar as notas digitadas?");
+};
